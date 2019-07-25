28ae17d80aa89e2248df11b249db47d1643bd163
liujiangjun <liujiangjun2014@163.com>
2019-07-25 09:19:26 +0800

    add scss

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../icons/wechat.svg';
+import './icon.scss';
 
 interface IconProps {
     name: string;
@@ -7,11 +8,9 @@ interface IconProps {
 
 const Icon: React.FunctionComponent<IconProps> = (props) => {
     return (
-        <span>
-            <svg>
-                <use xlinkHref="#wechat" />
+        <svg className="jui-icon">
+            <use xlinkHref={`#${props.name}`}/>
             </svg>
-        </span>
     )
 
 }
