92eef4d6640b7dbb60fb8e4b1e90386d3ebdfb9f
liujiangjun <liujiangjun2014@163.com>
2019-07-30 08:40:54 +0800

    onClick 类型完善

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -4,12 +4,13 @@ import './icon.scss';
 
 interface IconProps {
     name: string;
+    onClick: React.MouseEventHandler<SVGElement>
 }
 
 const Icon: React.FunctionComponent<IconProps> = (props) => {
     return (
         <svg className="jui-icon">
-            <use xlinkHref={`#${props.name}`}/>
+            <use xlinkHref={`#${props.name}`} onClick={props.onClick}/>
             </svg>
     )
 
