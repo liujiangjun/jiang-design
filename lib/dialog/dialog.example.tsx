10382bfe76190921a33fbb250d2d104879f10239
liujiangjun <liujiangjun2014@163.com>
2019-10-12 09:34:27 +0800

    完成 alert confirm modal

--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -1,18 +1,44 @@
-import React, {useState} from 'react';
-import Dialog from './dialog';
+import React, {useState, Fragment} from 'react';
+import Dialog, { alert, confirm, modal } from './dialog';
 
 const DialogExample: React.FunctionComponent = () => {
-    const [visible, setVisible] = useState(false)
+    const [x, setX] = useState(false)
+    const [y, setY] = useState(false)
+    const openModal = () => {
+        const onClose = modal(<button onClick={() => onClose()}>点我关闭</button>)
+    }
     return (
-        <div>
-            <button onClick={() => setVisible(!visible)}>按钮</button>
-            <Dialog
-                visible={visible}
-                buttons={[<button>1</button>, <button>2</button>]}
-            >
-                <div>111</div>
-            </Dialog>
-        </div>
+        <Fragment>
+            <div>
+                <h1>example1</h1>
+                <button onClick={() => setX(!x)}>按钮</button>
+                <Dialog
+                    visible={x}
+                    buttons={[<button onClick={() => setX(false)}>1</button>, <button>2</button>]}
+                    onClose={() => setX(false)}
+                >
+                    <div>111</div>
+                </Dialog>
+            </div>
+            <div>
+                <h1>example2</h1>
+                <button onClick={() => setY(!y)}>按钮</button>
+                <Dialog
+                    visible={y}
+                    buttons={[<button onClick={() => setY(false)}>1</button>, <button>2</button>]}
+                    onClose={() => setY(false)}
+                    closeOnClickMask={true}
+                >
+                    <div>111</div>
+                </Dialog>
+            </div>
+            <div>
+                <h1>example3</h1>
+                <button onClick={() => alert('alert')}>alert</button>
+                <button onClick={() => confirm('confirm', () => {console.log('yes')}, () => {console.log('no')})}>confirm</button>
+                <button onClick={openModal}>modal</button>
+            </div>
+        </Fragment>
     )
 }
 
--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,11 +1,14 @@
-import * as React from 'react';
+import React, {ReactElement, ReactFragment, ReactNode} from "react";
+import ReactDOM from 'react-dom';
 import './dialog.scss';
 import {scopedClassMaker} from "../helpers/classes";
 import {Icon} from "../index";
 
 interface Props {
     visible: boolean;
-    buttons: Array<React.ReactElement>;
+    buttons?: Array<React.ReactElement>;
+    onClose: React.MouseEventHandler;
+    closeOnClickMask?: boolean;
 }
 
 
@@ -13,27 +16,86 @@ const scopedClass = scopedClassMaker('jui-dialog')
 const sc = scopedClass
 
 
-const Dialog: React.FunctionComponent<Props> = ({visible, children, buttons}) => {
-    return (
+const Dialog: React.FunctionComponent<Props> = ({visible, children, buttons, onClose, closeOnClickMask}) => {
+
+    const handleClickClose: React.MouseEventHandler = (e) => {
+        onClose(e)
+    };
+
+    const dialog = (
         visible ?
             <React.Fragment>
-                <div className={sc('mask')}>
+                <div className={sc('mask')} onClick={e => !!closeOnClickMask && handleClickClose(e)}>
                 </div>
                 <div className={sc()}>
-                    <div className={sc('close')}>
+                    <div className={sc('close')} onClick={handleClickClose}>
                         <Icon name="close"/>
                     </div>
-                    <header className={sc('header')}>11</header>
+                    <header className={sc('header')}>header</header>
                     <main className={sc('main')}>
                         {children}
                     </main>
                     <footer className={sc('footer')}>
-                        {buttons}
+                        {buttons && buttons.map((item, index) => (
+                            React.cloneElement(item, {key: index})
+                        ))}
                     </footer>
                 </div>
             </React.Fragment>
             : null
     )
+    return (
+        ReactDOM.createPortal(dialog, document.body)
+    )
+
 }
+
+Dialog.defaultProps = {
+    closeOnClickMask: false
+}
+
+const modal = (content: ReactNode | ReactFragment, buttons?: Array<ReactElement>, afterClose?: () => void) => {
+    const div = document.createElement('div')
+    const handleClose = () => {
+        ReactDOM.render(React.cloneElement(component, {visible: false}), div)
+        ReactDOM.unmountComponentAtNode(div)
+        div.remove()
+    }
+    const component = (
+        <Dialog
+            visible={true}
+            onClose={() => {handleClose(); afterClose && afterClose()}}
+            buttons={buttons}
+        >
+            {content}
+        </Dialog>
+    )
+    document.body.append(div)
+    ReactDOM.render(component, div)
+    return handleClose
+}
+
+const alert = (content: string | number) => {
+    const onClose = modal(<div>我是 alert</div>, [<button onClick={() => onClose()}>OK</button>])
+}
+
+const confirm = (content: string, yes?: () => void, no?: () => void) => {
+    const onYes = () => {
+        onClose()
+        yes && yes()
+    }
+    const onNo = () => {
+        onClose()
+        no && no()
+    }
+    const buttons = [
+        <button onClick={onYes}>yes</button>,
+        <button onClick={onNo}>no</button>
+    ]
+    const onClose = modal(content, buttons, no)
+}
+
+
+export {alert, confirm, modal}
 
 export default Dialog
